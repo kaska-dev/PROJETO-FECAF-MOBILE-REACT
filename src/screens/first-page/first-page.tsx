4830d2dbe9adc6ff70fdc54f5debf52328fd855e
vinicius-somogyi <viniciussomogyi11@gmail.com>
2025-08-11 07:51:28 -0300

    .

--- src/screens/first-page/first-page.tsx
+++ src/screens/first-page/first-page.tsx
@@ -5,7 +5,7 @@ import { styles } from './styles'
 export const FirstPage = () => {
   return (
     <View style={styles.container}>
-      <Text>Olá</Text>
+      <Text>Olá a</Text>
     </View>
   )
 }
